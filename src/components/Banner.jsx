24aa175a815742250c53153b026ac23fcf2b5aa1
imafnan <afnanalamanan99@gmail.com>
2025-01-22 19:23:14 +0600

    ok ok ok

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,22 +4,24 @@ import Kiwi from "../assets/Image/Kiwi.png";
 import Bluberry from "../assets/Image/Bluberry.png";
 
 // =========== icon image import
-import bannaIcon from '../assets/Image/bananaIcon.png'
-import StrawberryIcon from '../assets/Image/StrawberryIcon.png'
-import KiwiIcon from '../assets/Image/KiwiIcon.png'
-import bluberryIcon from '../assets/Image/bluberryIcon.png'
+import bannaIcon from "../assets/Image/bananaIcon.png";
+import StrawberryIcon from "../assets/Image/StrawberryIcon.png";
+import KiwiIcon from "../assets/Image/KiwiIcon.png";
+import bluberryIcon from "../assets/Image/bluberryIcon.png";
 import { useState } from "react";
 
-
-
 const Banner = () => {
-    const [Rounded , setRounded] =useState('rotate-[-31deg]')
-    const [Color ,setColor ] =useState({bgcolor:'bg-[#FFF7CF]' ,divbg:'bg-[#F5DE6E]'})
-
+  const [Rounded, setRounded] = useState("rotate-[-31deg]");
+  const [Color, setColor] = useState({
+    divbg: "bg-[#F5DE6E]",
+    bgcolor: "bg-[#FFF7CF]",
+  });
 
   return (
     <>
-      <section className={`${Color.bgcolor} transition-all duration-[1s] relative h-screen pt-[180px]  overflow-hidden`}>
+      <section
+        className={`${Color.bgcolor} transition-all duration-[1s] relative h-screen pt-[180px]  overflow-hidden`}
+      >
         <div className="container">
           <div className="banner_row flex  ">
             <div className="banner_text">
@@ -31,17 +33,62 @@ const Banner = () => {
                 made with love, care, and the finest ingredients.
               </p>
               <div className="flex mt-[20px] mb-5">
-                <img onClick={()=>{setRounded('rotate-[-31deg]'), setColor((prev)=>({...prev ,bgcolor:'bg-[#FFF7CF]' ,divbg:'bg-[#F5DE6E]'}))}} src={bannaIcon} alt="bannaIcon" />
-                <img onClick={()=>{setRounded('rotate-[60deg]'), setColor((prev)=>({...prev ,bgcolor:'bg-[#FFE1E2]' ,divbg:'bg-[#FCB1B4]'}))}} src={StrawberryIcon} alt="StrawberryIcon" />
-                <img onClick={()=>{setRounded('rotate-[150deg]'),setColor((prev)=>({...prev ,bgcolor:'bg-[#ECFFCC]' ,divbg:'bg-[#8FB351]'}))}} src={KiwiIcon} alt="KiwiIcon" />
-                <img onClick={()=>{setRounded('rotate-[250deg]'), setColor((prev)=>({...prev ,bgcolor:'bg-[#FFCBEA]' ,divbg:'bg-[#C976A7]'}))}} src={bluberryIcon} alt="bluberryIcon" />
+                <img
+                  onClick={() => {
+                    setRounded("rotate-[-31deg]");
+                    setColor((prev) => ({
+                      ...prev,
+                      bgcolor: "bg-[#FFF7CF]",
+                      divbg: "bg-[#F5DE6E]",
+                    }));
+                  }}
+                  src={bannaIcon}
+                  alt="bannaIcon"
+                />
+                <img
+                  onClick={() => {
+                    setRounded("rotate-[60deg]");
+                    setColor((prev) => ({
+                      ...prev,
+                      bgcolor: "bg-[#FFE1E2]",
+                      divbg: "bg-[#FCB1B4]",
+                    }));
+                  }}
+                  src={StrawberryIcon}
+                  alt="StrawberryIcon"
+                />
+                <img
+                  onClick={() => {
+                    setRounded("rotate-[150deg]");
+                    setColor((prev) => ({
+                      ...prev,
+                      bgcolor: "bg-[#ECFFCC]",
+                      divbg: "bg-[#8FB351]",
+                    }));
+                  }}
+                  src={KiwiIcon}
+                  alt="KiwiIcon"
+                />
+                <img
+                  onClick={() => {
+                    setRounded("rotate-[250deg]");
+                    setColor((prev) => ({
+                      ...prev,
+                      bgcolor: "bg-[#FFCBEA]",
+                      divbg: "bg-[#C976A7]",
+                    }));
+                  }}
+                  src={bluberryIcon}
+                  alt="bluberryIcon"
+                />
               </div>
             </div>
 
             {/* ====================== Image Rotede part =====================*/}
 
-            <div className={`banner_foods ${Rounded} ${Color.divbg} transition-all duration-[1s] right-[-300px] bottom-[-550px]  absolute w-[1000px] h-[1000px] bg-bannaTwo rounded-full `}>
-              
+            <div
+              className={`banner_foods ${Rounded} ${Color.divbg} transition-all duration-[1s] right-[-300px] bottom-[-550px]  absolute w-[1000px] h-[1000px]  rounded-full `}
+            >
               <div className="banna absolute w-[600px] top-[-290px] left-[198px] ">
                 <img src={Banna} alt="banana" />
               </div>
@@ -58,7 +105,6 @@ const Banner = () => {
                 <img src={Bluberry} alt="Bluberry" />
               </div>
             </div>
-            
           </div>
         </div>
       </section>
